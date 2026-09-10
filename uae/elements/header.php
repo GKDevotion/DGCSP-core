7768ac2b923e79a16f00c1712737c60089f8a25a
GKDevotion <gk@devotiontech.io>
2026-09-10 14:35:06 +0530

    uae update common path

--- uae/elements/header.php
+++ uae/elements/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Devotion Global CSP</title>
+    <title>Devotion Global CSP | United Arab Emirates</title>
     
     <!-- Google Font: Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,9 +18,9 @@
     
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/singapore/assets/css/sg-style.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/sg-responsive.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/sg-keyframe.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/uae/assets/css/uae-style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/responsive.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/keyframe.css">
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -42,4 +42,4 @@
 </head>
 <body>
 
-    <?php include ROOT_PATH . '/singapore/elements/header-menu.php'; ?>
+    <?php include ROOT_PATH . '/uae/elements/header-menu.php'; ?>
--- uae/temp.php
+++ uae/temp.php
@@ -1,8 +1,8 @@
 <?php 
 require_once __DIR__ . '/../config.php';
-include ROOT_PATH . '/singapore/elements/header.php';
+include ROOT_PATH . '/uae/elements/header.php';
 
 ?>
 
 
-<?php include ROOT_PATH . '/singapore/elements/footer.php'; ?>
+<?php include ROOT_PATH . '/uae/elements/footer.php'; ?>
