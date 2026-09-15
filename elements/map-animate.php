3464a50871c160cc8b3d84617116305665917296
GKDevotion <gk@devotiontech.io>
2026-09-15 16:08:20 +0530

    update banner GIF image

--- elements/map-animate.php
+++ elements/map-animate.php
@@ -4,16 +4,16 @@
     <canvas id="canvas" class="map-animate"></canvas>
 
     <div class="container-xl cards-wrapper-relative mb-4">
-        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-3 g-lg-4 align-items-stretch">
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-3 g-lg-4 align-items-stretch pb-2">
             
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="200">
                     <div class="icon-container">
-                        <img src="assets/images/icons/country.png" alt="Globe Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/emerging-industries.gif' ?>" alt="Globe Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">10+ Countries &<br>Global Coverage</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">10+ Countries &<br>Global Coverage</h3>
                     </div>
                 </div>
             </div>
@@ -21,11 +21,11 @@
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="400">
                     <div class="icon-container">
-                        <img src="assets/images/icons/best-price.png" alt="Pricing Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/transparent-pricing.gif' ?>" alt="Pricing Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">Transparent<br>Pricing</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">Transparent<br>Pricing</h3>
                     </div>
                 </div>
             </div>
@@ -33,11 +33,11 @@
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="600">
                     <div class="icon-container">
-                        <img src="assets/images/icons/clock.png" alt="Clock Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/clock-icon.gif' ?>" alt="Clock Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">Fast Company<br>Setup</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">Fast Company<br>Setup</h3>
                     </div>
                 </div>
             </div>
@@ -45,11 +45,11 @@
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="800">
                     <div class="icon-container">
-                        <img src="assets/images/icons/cyber-security.png" alt="Security Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/secure-confidential.gif' ?>" alt="Security Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">Secure &<br>Confidential</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">Secure &<br>Confidential</h3>
                     </div>
                 </div>
             </div>
@@ -57,11 +57,11 @@
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="1000">
                     <div class="icon-container">
-                        <img src="assets/images/icons/customer-support.png" alt="Support Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/expert-support.gif' ?>" alt="Support Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">Expert Business<br>Support</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">Expert Business<br>Support</h3>
                     </div>
                 </div>
             </div>
@@ -69,11 +69,11 @@
             <div class="col">
                 <div class="glass-card text-center" data-aos="fade-up" data-aos-delay="1200">
                     <div class="icon-container">
-                        <img src="assets/images/icons/24-hours-support.png" alt="24/7 Icon" class="card-icon">
+                        <img src="<?=  BASE_URL.'/assets/images/gif/24-7-support.gif' ?>" alt="24/7 Icon" class="card-icon">
                     </div>
                     <div class="card-body-content w-100">
                         <div class="gold-divider"></div>
-                        <h3 class="fw-bold fs-6 mt-2 text-dark mb-0 brand-font">24 x 7<br>Support Center</h3>
+                        <h3 class="fw-bold fs-6 mt-2 mb-2 text-dark mb-0 brand-font">24 x 7<br>Support Center</h3>
                     </div>
                 </div>
             </div>
